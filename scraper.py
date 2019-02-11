c3c0b7da36b8d730584f467d5b9e2adf13f80377
EricTalv <eric.talviste@tptlive.ee>
2019-02-11 20:00:37 +0200

    fetched data from url

--- scraper.py
+++ scraper.py
@@ -1,11 +1,17 @@
 # OBJECTIVE: Fetch html data from any given link
 # METHOD: Inspect-Element
 
-#Import libraries
+# Import libraries
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 
-#Lets get the URL as client input
-link = input("Insert a Link")
-print(link)
+# Lets get the URL as client input
+#link = input("Insert a Link: ")
+link = 'https://veebisekretar.ee/'
 
+# Open url and store it 
+html = urlopen(link)
+
+soup = BeautifulSoup(html, 'html.parser')
+
+print(soup)
