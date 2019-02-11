43d830841fc042bb266ccf5b78b882d83705f58e
EricTalv <eric.talviste@tptlive.ee>
2019-02-11 19:49:21 +0200

    InspectElement method of getting data

--- scraper.py
+++ scraper.py
@@ -1,1 +1,11 @@
 # OBJECTIVE: Fetch html data from any given link
+# METHOD: Inspect-Element
+
+#Import libraries
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+
+#Lets get the URL as client input
+link = input("Insert a Link")
+print(link)
+
