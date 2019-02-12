2e266bbf8e938fef3c7a965fd441f0962051942f
Eric Talviste <Eric.Talviste@tptlive.ee>
2019-02-12 16:12:59 +0200

    Error handeling

--- scraper.py
+++ scraper.py
@@ -5,15 +5,39 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 
+from requests import get
+from requests.exceptions import RequestException
+from contextlib import closing
+
 # Lets get the URL as client input
 #link = input("Insert a Link: ")
 link = 'https://veebisekretar.ee/'
 
-# Open URL request on link 
-req = urlopen(link)
+# Request data Function
+def simple_get(url):
+   
+    # Attempts to get the content at `url` by making an HTTP GET request.
+    try:
+        with closing(get(url, stream=True)) as resp:
+            if is_good_response(resp):
+                return resp.content
+            else:
+                return None
 
-# Parse response HTML data 
-res = BeautifulSoup(req, 'html.parser')
+    except RequestException as e:
+        log_error('Error during requests to {0} : {1}'.format(url, str(e)))
+        return None
 
-# Print response data 
-print('Success \n', res)
+def is_good_response(resp):
+    # Returns True if the response seems to be HTML, False otherwise.
+    content_type = resp.headers['Content-Type'].lower()
+    return (resp.status_code == 200 
+            and content_type is not None 
+            and content_type.find('html') > -1)
+
+def log_error(e):
+    # Print any errors
+    print(e)
+
+
+simple_get(link)   
