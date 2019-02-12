6ecba6b08fc002ee0b08dbdb00d189a6f25a203c
Eric Talviste <Eric.Talviste@tptlive.ee>
2019-02-12 17:23:51 +0200

    Checking premade examples

--- scraper.py
+++ scraper.py
@@ -2,45 +2,66 @@
 # METHOD: Inspect-Element
 
 # Import libraries
-from urllib.request import urlopen
+import re
+import requests
+import requests.exceptions
+from urllib.parse import urlsplit
+from collections import deque
 from bs4 import BeautifulSoup
-
-from requests import get
-from requests.exceptions import RequestException
-from contextlib import closing
 
 # Lets get the URL as client input
 #link = input("Insert a Link: ")
 link = 'https://veebisekretar.ee/'
 
-# Request data Function
-def simple_get(url):
-   
-    # Attempts to get the content at `url` by making an HTTP GET request.
+# starting url. replace google with your own url.
+starting_url = 'https://veebisekretar.ee/'
+
+# a queue of urls to be crawled
+unprocessed_urls = deque([starting_url])
+
+# set of already crawled urls for email
+processed_urls = set()
+
+# a set of fetched emails
+emails = set()
+
+# process urls one by one from unprocessed_url queue until queue is empty
+while len(unprocessed_urls):
+
+    # move next url from the queue to the set of processed urls
+    url = unprocessed_urls.popleft()
+    processed_urls.add(url)
+
+    # extract base url to resolve relative links
+    parts = urlsplit(url)
+    base_url = "{0.scheme}://{0.netloc}".format(parts)
+    path = url[:url.rfind('/')+1] if '/' in parts.path else url
+
+    # get url's content
+    print("Crawling URL %s" % url)
     try:
-        with closing(get(url, stream=True)) as resp:
-            if is_good_response(resp):
-                return resp.content
-            else:
-                return None
+        response = requests.get(url)
+    except (requests.exceptions.MissingSchema, requests.exceptions.ConnectionError):
+        # ignore pages with errors and continue with next url
+        continue
 
-    except RequestException as e:
-        log_error('Error during requests to {0} : {1}'.format(url, str(e)))
-        return None
+    # extract all email addresses and add them into the resulting set
+    # You may edit the regular expression as per your requirement
+    new_emails = set(re.findall(r"[a-z0-9\.\-+_]+@[a-z0-9\.\-+_]+\.[a-z]+", response.text, re.I))
+    emails.update(new_emails)
+    print(emails)
+    # create a beutiful soup for the html document
+    soup = BeautifulSoup(response.text, 'lxml')
 
-def is_good_response(resp):
-    # Returns True if the response seems to be HTML, False otherwise.
-    content_type = resp.headers['Content-Type'].lower()
-    return (resp.status_code == 200 
-            and content_type is not None 
-            and content_type.find('html') > -1)
-
-def log_error(e):
-    # Print any errors
-    print(e)
-
-
-html_data = simple_get(link)
-
-print(html_data)
-
+    # Once this document is parsed and processed, now find and process all the anchors i.e. linked urls in this document
+    for anchor in soup.find_all("a"):
+        # extract link url from the anchor
+        link = anchor.attrs["href"] if "href" in anchor.attrs else ''
+        # resolve relative links (starting with /)
+        if link.startswith('/'):
+            link = base_url + link
+        elif not link.startswith('http'):
+            link = path + link
+        # add the new url to the queue if it was not in unprocessed list nor in processed list yet
+        if not link in unprocessed_urls and not link in processed_urls:
+            unprocessed_urls.append(link)
