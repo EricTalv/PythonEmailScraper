ed84d31320dda0afd9d0a612d4ab93beaf09b42d
Eric Talviste <Eric.Talviste@tptlive.ee>
2019-02-15 17:28:48 +0200

    Fixed timeout

--- urlerror_handle.py
+++ urlerror_handle.py
@@ -37,13 +37,24 @@ t.start()
 # Run Request
 try:
     res = requests.get(bad_url, timeout=0.001)
-    done = True
     print(res)
-except requests.exceptions.ConnectionError as err:
-    done = True
+except requests.exceptions.ConnectionError as e:
+    print("\n[ERROR]Connection Error:")
+    print(e)
+except requests.TooManyRedirects as e:   
+    print("\n[ERROR]Too many Redirects:")
+    print(e)
+except requests.Timeout as e:   
+    print("\n[ERROR]Connection Timeout:")
+    print(e)
+except requests.HTTPError as e:   
     print("\n[ERROR]")
-    print(err)
-
+    print(e)
+    sys.exit(1)
+except requests.RequestException as e:   
+    print("\n[ERROR]")
+    print(e)
+    sys.exit(1)    
 finally:
     done = True
     
