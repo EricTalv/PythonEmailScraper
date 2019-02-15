cf5e11f1c169d5a8a0dca4e92d7c03cf562fabf7
Eric Talviste <Eric.Talviste@tptlive.ee>
2019-02-15 13:08:16 +0200

    Timeout error

--- scraper.py
+++ scraper.py
@@ -5,11 +5,13 @@ import requests.exceptions
 import csv
 import signal
 import sys
+import eventlet
 from urllib.parse import urlsplit
 from collections import deque
 from bs4 import BeautifulSoup
 from colorama import *
 init()
+eventlet.monkey_patch()
 
 # starting url. replace google with your own url.
 # starting_url = 'https://erictalv.github.io./' #'https://www.neti.ee/cgi-bin/teema/ARI/Byrooteenused/'
@@ -86,7 +88,6 @@ def end_scene():
     else:
          print("Not Saved | Session Ended.")
          sys.exit()
-
 # process urls one by one from unprocessed_url queue until queue is empty
 while len(unprocessed_urls):
 
@@ -101,7 +102,7 @@ while len(unprocessed_urls):
 
     # get url's content
     print(Fore.CYAN + "Crawling URL %s" % url + Fore.WHITE)
-    try:
+    try:        
         response = requests.get(url)
     except (requests.exceptions.MissingSchema, requests.exceptions.ConnectionError):
         print(Back.RED + '[ERROR]Connection Error' + Back.BLACK)
