967b10d05daf3821b348a25c8ef98e25499beefd
Eric Talviste <Eric.Talviste@tptlive.ee>
2019-02-15 20:28:19 +0200

    signl might help

--- scraper.py
+++ scraper.py
@@ -14,6 +14,23 @@ from bs4 import BeautifulSoup
 from colorama import *
 init()
 
+# starting url. replace google with your own url.
+# starting_url = 'https://erictalv.github.io./' #'https://www.neti.ee/cgi-bin/teema/ARI/Byrooteenused/'
+
+# UNCOMMENT THIS ON PRODUCTION
+print(Back.BLUE + "|~~~~Email Scraper v2~~~~|" )
+starting_url = input("Enter Website Link to Scrape: ")
+print("Starting scraper.." + Back.BLACK)
+
+# a queue of urls to be crawled
+unprocessed_urls = deque([starting_url])
+
+# set of already crawled urls for email
+processed_urls = set()
+
+# a set of fetched emails
+emails = set()
+
 # A fancy bar
 def Bar(string_to_expand, length):
     return (string_to_expand * (int(length/len(string_to_expand))+1))[:length]
@@ -72,100 +89,87 @@ def end_scene():
     else:
          print("Not Saved | Session Ended.")
          sys.exit()
-         
-try:
-    # UNCOMMENT THIS ON PRODUCTION
-    print(Back.BLUE + "|~~~~Email Scraper v2~~~~|" )
-    starting_url = input("Enter Website Link to Scrape: ")
-    print("Starting scraper.." + Back.BLACK)
 
-    # a queue of urls to be crawled
-    unprocessed_urls = deque([starting_url])
+# process urls one by one from unprocessed_url queue until queue is empty
+while len(unprocessed_urls):
 
-    # set of already crawled urls for email
-    processed_urls = set()
+    # move next url from the queue to the set of processed urls
+    url = unprocessed_urls.popleft()
+    processed_urls.add(url)
 
-    # a set of fetched emails
-    emails = set()
-   
-    # process urls one by one from unprocessed_url queue until queue is empty
-    while len(unprocessed_urls):
+    # extract base url to resolve relative links
+    parts = urlsplit(url)
+    base_url = "{0.scheme}://{0.netloc}".format(parts)
+    path = url[:url.rfind('/')+1] if '/' in parts.path else url
 
-        # move next url from the queue to the set of processed urls
-        url = unprocessed_urls.popleft()
-        processed_urls.add(url)
-
-        # extract base url to resolve relative links
-        parts = urlsplit(url)
-        base_url = "{0.scheme}://{0.netloc}".format(parts)
-        path = url[:url.rfind('/')+1] if '/' in parts.path else url
-
-        # get url's content
-        print(Fore.CYAN + "Crawling URL %s" % url + Fore.WHITE) 
-        try:       
-            response = requests.get(url, timeout=3)
-            done = True
-        except requests.exceptions.ConnectionError as e:
-            print("\n[ERROR]Connection Error:")
-            print(e)
-            continue
-        except requests.Timeout as e:   
-            print("\n[ERROR]Connection Timeout:")
-            print(e)
-            continue
-        except requests.HTTPError as e:   
-            print("\n[ERROR]HTTP Error:")
-            print(e)
-            continue
-        except requests.RequestException as e:   
-            print("\n[ERROR]General Error:")
-            print(e)
-            continue    
-            # Check for CTRL+C interruption
-        except KeyboardInterrupt:
-            if len(emails) is 0:
-                print("No emails have been collected |Crawling Ended")
-            else:
-                end_scene()
-
-        # extract all email addresses and add them into the resulting set
-        new_emails = set(re.findall(r"[a-z0-9\.\-+_]+@[a-z0-9\.\-+_]+\.[a-z]+", response.text, re.I))
-        emails.update(new_emails)
-        
-        if len(new_emails) is 0:
-            print(Back.RED)        
-            print("No emails found")
-            print("URLS: {processed}/{unprocessed}".format(processed=str(len(processed_urls)),
-                                                           unprocessed=str(len(unprocessed_urls))))
-            print(Back.BLACK)
-        else:
-            print(Fore.GREEN) 
-            print(new_emails)
-            print("Email Count: ", len(emails))
-            print("URLS: {processed}/{unprocessed}".format(processed=str(len(processed_urls)),
-                                                           unprocessed=str(len(unprocessed_urls))))
-            print(Fore.WHITE)
-        
-        # create a beutiful soup for the html document
-        soup = BeautifulSoup(response.text, 'lxml')
-
-        # Once this document is parsed and processed, now find and process all the anchors i.e. linked urls in this document
-        for anchor in soup.find_all("a"):
-            # extract link url from the anchor
-            link = anchor.attrs["href"] if "href" in anchor.attrs else ''
-            # resolve relative links (starting with /)
-            if link.startswith('/'):
-                link = base_url + link
-            elif not link.startswith('http'):
-                link = path + link    
-                # add the new url to the queue if it was not in unprocessed list nor in processed list yet
-                if not link in unprocessed_urls and not link in processed_urls:
-                    unprocessed_urls.append(link)
+    # get url's content
+    print(Fore.CYAN + "Crawling URL %s" % url + Fore.WHITE) 
+    try:       
+        response = requests.get(url, timeout=3)
+        done = True
+    except requests.exceptions.ConnectionError as e:
+        print("\n[ERROR]Connection Error:")
+        print(e)
+        continue
+    except requests.Timeout as e:   
+        print("\n[ERROR]Connection Timeout:")
+        print(e)
+        continue
+    except requests.HTTPError as e:   
+        print("\n[ERROR]HTTP Error:")
+        print(e)
+        continue
+    except requests.RequestException as e:   
+        print("\n[ERROR]General Error:")
+        print(e)
+        continue    
+        # Check for CTRL+C interruption
+    except KeyboardInterrupt:
         if len(emails) is 0:
             print("No emails have been collected |Crawling Ended")
         else:
             end_scene()
-except KeyboardInterrupt:
- end_scene()
+            
+
+    # extract all email addresses and add them into the resulting set
+    # You may edit the regular expression as per your requirement
+    new_emails = set(re.findall(r"[a-z0-9\.\-+_]+@[a-z0-9\.\-+_]+\.[a-z]+", response.text, re.I))
+    emails.update(new_emails)
+    
+    if len(new_emails) is 0:
+        print(Back.RED)        
+        print("No emails found")
+        print("URLS: {processed}/{unprocessed}".format(processed=str(len(processed_urls)),
+                                                       unprocessed=str(len(unprocessed_urls))))
+        print(Back.BLACK)
+    else:
+        print(Fore.GREEN) 
+        print(new_emails)
+        print("Email Count: ", len(emails))
+        print("URLS: {processed}/{unprocessed}".format(processed=str(len(processed_urls)),
+                                                       unprocessed=str(len(unprocessed_urls))))
+        print(Fore.WHITE)
+    
+    # create a beutiful soup for the html document
+    soup = BeautifulSoup(response.text, 'lxml')
+
+    # Once this document is parsed and processed, now find and process all the anchors i.e. linked urls in this document
+    for anchor in soup.find_all("a"):
+        # extract link url from the anchor
+        link = anchor.attrs["href"] if "href" in anchor.attrs else ''
+        # resolve relative links (starting with /)
+        if link.startswith('/'):
+            link = base_url + link
+        elif not link.startswith('http'):
+            link = path + link
+        # add the new url to the queue if it was not in unprocessed list nor in processed list yet
+        if not link in unprocessed_urls and not link in processed_urls:
+            unprocessed_urls.append(link)
+
+if len(emails) is 0:
+    print("No emails have been collected |Crawling Ended")
+else:
+    end_scene()
+
 
 
