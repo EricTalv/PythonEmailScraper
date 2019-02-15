e06412a9b2c2566622b8f7b6d7eb31a0d5a4b401
Eric Talviste <Eric.Talviste@tptlive.ee>
2019-02-15 10:43:02 +0200

    Adding directory

--- csvwriting.py
+++ csvwriting.py
@@ -2,21 +2,26 @@ import csv
 
 emails = {"very@cool.net", "not@cool.eu", "email@cool.com"}
 
+directory = C:\Users\Eric\Desktop\emailcsvs\\
+
 print("Starting")
 # CSV Writer function
-def write_csv(file_name, collection):
+def write_csv(dirpath, file_name, collection):
     print("Open writer")
-    # Open writer, set file_name
-    with open(file_name + '.csv', 'w') as email_file:
+    # Open writer, set file_name and path
+    with open(dirpath + file_name + '.csv', 'w') as email_file:
         print("Pass Writer")
         # Pass email_file to writer
-        writer = csv.writer(email_file)
+        writer = csv.writer(email_file,
+                            delimiter=' ',
+                            quotechar='|',
+                            quoting=csv.QUOTE_MINIMAL)
 
         print("Writing throught collections:")
         # For every item inside the collection write a row
         for row in collection:
             print(row)
-            writer.writerow(row)
+            writer.writerow([row])
 
-write_csv('cool', emails)
+write_csv(directory,'cool2', emails)
 print("End.")
