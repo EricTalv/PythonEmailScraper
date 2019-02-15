257656cd7eb1a47b439c2c4607f29237736de707
Eric Talviste <Eric.Talviste@tptlive.ee>
2019-02-15 10:43:50 +0200

    Bugs

--- csvwriting.py
+++ csvwriting.py
@@ -2,7 +2,7 @@ import csv
 
 emails = {"very@cool.net", "not@cool.eu", "email@cool.com"}
 
-directory = C:\Users\Eric\Desktop\emailcsvs\\
+directory = 'C:\Users\Eric\Desktop\emailcsvs\\'
 
 print("Starting")
 # CSV Writer function
