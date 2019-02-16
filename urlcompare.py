de7a913ff53a93c0ae39a2ed22334ac9df25eb5c
EricTalv <eric.talviste@tptlive.ee>
2019-02-16 21:31:47 +0200

    BlockedSites file tests2

--- urlcompare.py
+++ urlcompare.py
@@ -3,6 +3,6 @@
 urls = {"facebook.com", "vikipedia", "wikipedia"}
 
 with open('blocked_sites.txt','r') as blocked_sites:
-   readlines = blocked_sites.readlines()
-   for row in readlines:
+   lines = blocked_sites.readlines()
+   for row in lines:
       print(row)
