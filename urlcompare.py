2f58ca40e8b554f475e531e71559fda5b09fd437
EricTalv <eric.talviste@tptlive.ee>
2019-02-17 17:28:28 +0200

    Contains tests

--- urlcompare.py
+++ urlcompare.py
@@ -1,14 +1,23 @@
 
 
-urls = {"facebook.com", "vikipedia", "wikipedia"}
+urls = {"facebook.com", "vikipedia", "wikipedia", "gold"}
 
 # Open blocked sites list
 with open('blocked_sites.txt','r') as blocked_sites:
    # Read all line from file
    blocked = blocked_sites.readlines()
    # Compare checks
-   for row in blocked:
-      # Compare lines List to Urls
+   ## For each URL in URLS
+   for url in urls:
+      ## and For each BLOCKER in BLOCKED
+      for blocker in blocked:
+         ## IF URL is in BLOCKER
+         if url in blocker:
+            print("URL:{url} | BLOCKER:{blocker}".format(url=url,
+                                                         blocker=blocker))
+         else:
+            print("no match")
+     
          # If URLS Contains ITEM in BLOCKED
            # Remove ITEM
 
