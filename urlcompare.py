a365e0696731a31a482e2dcd5a0808e9969f3a6b
EricTalv <eric.talviste@tptlive.ee>
2019-02-17 17:51:10 +0200

    Theres an err with sets likely

--- urlcompare.py
+++ urlcompare.py
@@ -4,25 +4,15 @@ urls = {'facebook.com', 'youtube.com', 'gold'}
 
 # Open blocked sites list
 with open('blocked_sites.txt','r') as blocked_sites:
-   # Read all line from file
-   blocked = blocked_sites.readlines()
-   # Compare checks
-   ## For each URL in URLS
+   # For every URL in URLS
    for url in urls:
-      ## and For each BLOCKER in BLOCKED
-      for blocker in blocked:
-         ## IF URL is in BLOCKER
-          if blocker in url:
-               print("[M]URL:{url} = BLOCKER:{blocker}".format(url=url,
-                                                               blocker=blocker))
-          else:
-               print("[N]URL:{url} | BLOCKER:{blocker}".format(url=url,
-                                                               blocker=blocker))
-     
-         # If URLS Contains ITEM in BLOCKED
-           # Remove ITEM
-
-         
-      # check if URLS has any BLOCKED urls
-      # Remove the URLS that are TRUE
-
+      # For every BLOCKER in BLOCKED_SITES
+      for blocker in blocked_sites:
+         # If url CONTAINS Blocker
+         if blocker in url:
+              print("[M]URL:{url} == BLOCKER:{blocker}".format(url=url,
+                                                              blocker=blocker))
+         else:
+              print("[N]URL:{url} || BLOCKER:{blocker}".format(url=url,
+                                                              blocker=blocker))
+    
