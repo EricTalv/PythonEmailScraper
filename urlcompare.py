e4a111f7b908a54ec4f2b1374887b020b95ddf27
EricTalv <eric.talviste@tptlive.ee>
2019-02-17 18:23:58 +0200

    Compare function Made

--- urlcompare.py
+++ urlcompare.py
@@ -2,17 +2,19 @@
 
 urls = {'facebook.com', 'youtube.com', 'gold'}
 
+blocked = set(line.strip() for line in open('blocked_sites.txt'))
+
 # Open blocked sites list
-with open('blocked_sites.txt','r') as blocked_sites:
-   # For every URL in URLS
-   for url in urls:
-      # For every BLOCKER in BLOCKED_SITES
-      for blocker in blocked_sites:
-         # If url CONTAINS Blocker
-         if blocker in url:
-              print("[M]URL:{url} == BLOCKER:{blocker}".format(url=url,
-                                                              blocker=blocker))
-         else:
-              print("[N]URL:{url} || BLOCKER:{blocker}".format(url=url,
-                                                              blocker=blocker))
-    
+#with open('blocked_sites.txt','r') as blocked_sites:  
+# For every URL in URLS
+for url in urls:
+   # For every BLOCKER in BLOCKED_SITES
+   for blocker in blocked:
+      # If url CONTAINS Blocker
+      if blocker in url:
+           print("[M]URL:{url} == BLOCKER:{blocker}".format(url=url,
+                                                           blocker=blocker))
+      else:
+           print("[N]URL:{url} || BLOCKER:{blocker}".format(url=url,
+                                                           blocker=blocker))
+ 
