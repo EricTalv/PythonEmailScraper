f9148e189c92e7d0da63f5a7ad52c3e450fc50a6
EricTalv <eric.talviste@tptlive.ee>
2019-02-18 23:48:34 +0200

    Sets are lists but lists work differently from sets so sets arent really litsts but no sets are lists what

--- itemremover.py
+++ itemremover.py
@@ -1,13 +1,11 @@
 
-urls = {'facebook.com', 'youtube.com', 'gold'}
-blocked = {'facebook'}
+urls = ['facebook.com', 'youtube.com', 'gold']
+blocked = ['facebook', 'youtube']
 
 for url in urls:
    for blocker in blocked:      
       if blocker in url:
          urls.remove(url)
          print('removed: ' + url)
-      else:
-         print('no item')
-
+print(urls)
 
