b3856b4af41138cc7ae64fcfdde796bad109c84f
EricTalv <eric.talviste@tptlive.ee>
2019-02-19 20:59:52 +0200

    Running timeit tests

--- itemremover.py
+++ itemremover.py
@@ -1,11 +1,14 @@
+from timeit import default_timer as timer
 
 urls = ['facebook.com', 'youtube.com', 'gold']
 blocked = ['facebook', 'netflix','youtube']
 
-for url in urls:
-   for blocker in blocked:      
-      if blocker in url:
-         urls.remove(url)
-         print('removed: ' + url)
-print(urls)
+def remover():      
+   for url in urls:
+      for blocker in blocked:      
+         if blocker in url:
+            urls.remove(url)
+            print('removed: ' + url)
+
+
 
