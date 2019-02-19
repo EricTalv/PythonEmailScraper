15c6e75480b35d253b35f5e3dee04b0eb7c44516
EricTalv <eric.talviste@tptlive.ee>
2019-02-19 21:07:23 +0200

    TimeIT works, checking on item remover

--- itemremover.py
+++ itemremover.py
@@ -1,14 +1,28 @@
 from timeit import default_timer as timer
 
-urls = ['facebook.com', 'youtube.com', 'gold']
-blocked = ['facebook', 'netflix','youtube']
+processed_urls = set()
+unprocessed_urls = {'facebook.com', 'youtube.com', 'gold'}
+blockers = {'facebook', 'netflix','youtube'}
 
 def remover():      
-   for url in urls:
-      for blocker in blocked:      
+   for url in unprocessed_urls:
+      for blocker in blockers:      
          if blocker in url:
-            urls.remove(url)
-            print('removed: ' + url)
+            processed_urls.add(url)
+            print('moved: ' + url)
 
 
 
+start = timer()
+
+remover()
+
+end = timer()
+
+print(end - start)
+print("P_URLS: ")
+print(processed_urls)
+print("U_URLS: ")
+print(unprocessed_urls)
+print("Difference: ")
+print(unprocessed_urls.difference(processed_urls))
